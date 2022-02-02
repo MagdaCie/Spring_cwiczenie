66e25873b2909de6f429c78b7baacfada5545af8
noskowiak <97466059+noskowiak@users.noreply.github.com>
2022-02-02 18:47:23 +0100

    Hibernate

--- hibernate/src/main/java/sda/hibernate/model/Country.java
+++ hibernate/src/main/java/sda/hibernate/model/Country.java
@@ -3,24 +3,20 @@ package sda.hibernate.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
-
 public class Country {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CO_ID")
     private int id;
+    @Column(name = "CO_NAME")
     private String name;
+    @Column(name = "CO_ALIAS")
     private String alias;
-
-
-
 
 }
