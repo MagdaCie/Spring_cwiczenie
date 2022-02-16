ff4834371652ebf55e39aa0d01ab39c9e9c18d8d
noskowiak <97466059+noskowiak@users.noreply.github.com>
2022-02-16 19:48:41 +0100

    findNonCriteriaIsAllWhoBoughtProduct

--- hibernate/src/main/java/sda/hibernate/dao/UserCriteriaDao.java
+++ hibernate/src/main/java/sda/hibernate/dao/UserCriteriaDao.java
@@ -2,13 +2,11 @@ package sda.hibernate.dao;
 
 import org.hibernate.Session;
 import sda.hibernate.model.*;
+import sda.hibernate.model.Order;
 import sda.hibernate.util.HibernateUtil;
 
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -32,7 +30,7 @@ public class UserCriteriaDao {
         //"where"
         criteriaQuery.select(userRoot).where(
                 //user.lastName like s
-                criteriaBuilder.like(userRoot.get(User_.lastName), "%"+s+"%")
+                criteriaBuilder.like(userRoot.get(User_.lastName), "%" + s + "%")
         );
         Query query = session.createQuery(criteriaQuery);
         List<User> users = query.getResultList();
@@ -44,15 +42,41 @@ public class UserCriteriaDao {
         return null;
     }
 
-    public List<User> findAllByCountryAlias(String alias){
-        Root<User> userRoot = getRoot();
-        Join<User, Address> addressJoin = userRoot.join(User_.address);
-        Join<Address, Country> countryJoin = addressJoin.join(Address_.country);
-        criteriaQuery.select(userRoot).where(criteriaBuilder.equal(countryJoin.get(Country_.alias), alias));
+    public List<User> findAllByCountryAlias(String alias) {
+        Root<User> userRoot = getRoot(); // SELECT * FROM `User`
+        Join<User, Address> addressJoin = userRoot.join(User_.address); //join Address on USR_ADD_ID = ADD_ID
+        Join<Address, Country> countryJoin = addressJoin.join(Address_.country); //join Country on ADD_CO_ID = CO_ID
+        criteriaQuery.select(userRoot).where(criteriaBuilder.equal(countryJoin.get(Country_.alias), alias)); //where CO_ALIAS = ?
         Query query = session.createQuery(criteriaQuery);
         List<User> users = query.getResultList();
         session.close();
         return users;
+
+    }
+
+    public List<User> findAllWhoBoughtProduct(Product product) {
+        Root<User> userRoot = getRoot();
+        Join<User, Order> orderJoin = userRoot.join(User_.orders);
+        Join<Order, Product> productJoin = orderJoin.join(Order_.PRODUCTS);
+        criteriaQuery.select(userRoot).where(criteriaBuilder.equal(productJoin, product));
+        Query query = session.createQuery(criteriaQuery);
+        List<User> users = query.getResultList();
+        session.close();
+        return users;
+    }
+
+    public List<User> findNonCriteriaIsAllWhoBoughtProduct(Product product) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery(
+                "select u from User u " +
+                        "join u.orders o " +
+                        "join o.products p " +
+                        "where p = :product",
+                User.class
+        ).setParameter("product", product);
+        List<User> productList = query.getResultList();
+        session.close();
+        return productList;
     }
 
 }
