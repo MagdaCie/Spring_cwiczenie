b8d718e0278e864023c22acd6ebc33a06c17b6dd
noskowiak <97466059+noskowiak@users.noreply.github.com>
2022-03-29 18:31:46 +0200

    BookControlerAdvice

---
+++ Spring_cwiczenie/src/main/java/pl/sda/spring/spring_cwiczenie/web/BookControlerAdvice.java
@@ -0,0 +1,18 @@
+package pl.sda.spring.spring_cwiczenie.web;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BookControlerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    public String handleIndex(IndexOutOfBoundsException e) {
+        return "This is out of bound exception" + e.getMessage();
+    }
+}
